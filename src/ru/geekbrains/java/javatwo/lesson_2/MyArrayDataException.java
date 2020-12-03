1138b62491e4cce3c1bdc0d4181c9a4d6a0000b9
orqu3 <marisergiv@gmail.com>
2020-12-03 11:21:19 +0300

    Adjustments according to the notes (solution for lesson 2)

--- src/ru/geekbrains/java/javatwo/lesson_2/MyArrayDataException.java
+++ src/ru/geekbrains/java/javatwo/lesson_2/MyArrayDataException.java
@@ -2,7 +2,7 @@ package ru.geekbrains.java.javatwo.lesson_2;
 
 public class MyArrayDataException extends RuntimeException {
 
-    public MyArrayDataException() {
-        super("It must be an integer!");
+    public MyArrayDataException(String value, int i, int j) {
+        super("Invalid value " + value + " in array cell " + "[" + i + "][" + j + "]! It must be integer!");
     }
 }
